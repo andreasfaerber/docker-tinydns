403a14b63bf80358f56a62c96676d2e17539e500
Andreas Faerber <af@maeh.org>
2016-05-22 19:19:54 +0200

    Initial version

---
+++ dnsdata_update.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd /
+while /bin/true
+do
+  date
+  # Ignore DEPRECATED due to docker gogs image that needs updating
+  cd /etc/tinydns/root && git pull 2>&1 | egrep -ve "(Already up-to-date|DEPRECATED)"
+  make 2>&1 | grep -v "is up to date."
+  cd /
+  sleep $GIT_UPDATE_FREQUENCY
+done
---
+++ svscan.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cat >> /root/.ssh/config <<EOF
+StrictHostKeyChecking no
+EOF
+
+cd /etc/tinydns/root
+if [ ! -d .git ]; then
+    cd /
+    rm -rf /etc/tinydns/root/* /etc/tinydns/root/.git
+    cd /etc/tinydns/root
+    git clone $GIT_DNSDATA .
+fi
+
+# Runs git pull && make in a while loop to avoid cron installation
+/dnsdata_update.sh 2>&1 &
+
+svscan /etc/service
