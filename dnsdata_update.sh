5e544b0fc82290efbd81ce3dbc527302f5eb0b89
Andreas Faerber (root@d1.maeh.org) <af@maeh.org>
2016-05-24 16:06:38 +0200

    Also update axfrdns tcp file during domain data updates

--- dnsdata_update.sh
+++ dnsdata_update.sh
@@ -7,6 +7,6 @@ do
   # Ignore DEPRECATED due to docker gogs image that needs updating
   cd /etc/tinydns/root && git pull 2>&1 | egrep -ve "(Already up-to-date|DEPRECATED)"
   make 2>&1 | grep -v "is up to date."
-  cd /
+  cd /etc/axfrdns && make
   sleep $GIT_UPDATE_FREQUENCY
 done
